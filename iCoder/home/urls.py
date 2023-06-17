e599157d1e22027fa4d5d463e612dcb8a2ea92ff
Niraj <neeraz.pandey2058@gmail.com>
2023-06-17 22:32:59 +0545

    Added Login and SignUp Modals and created users.

--- iCoder/home/urls.py
+++ iCoder/home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('search/', views.search, name='search')
+    path('search/', views.search, name='search'),
+    path('signup', views.handleSignup, name='handleSignup')
 ]
