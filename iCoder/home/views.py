13a2f4eb15f2a64ba6ad672a877349efe9f4e62e
Niraj <neeraz.pandey2058@gmail.com>
2023-06-17 22:40:10 +0545

    Added user validation.

--- iCoder/home/views.py
+++ iCoder/home/views.py
@@ -51,6 +51,19 @@ def handleSignup(request):
         pass1=request.POST['pass1']
         pass2=request.POST['pass2']
 
+        # User Validation
+        if len(username)<10:
+            messages.error(request, " Your user name must be under 10 characters")
+            return redirect('home')
+
+        if not username.isalnum():
+            messages.error(request, " User name should only contain letters and numbers")
+            return redirect('home')
+        if (pass1!= pass2):
+             messages.error(request, " Passwords do not match")
+             return redirect('home')
+        
+
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name= fname
         myuser.last_name= lname
