c5938d522c66843bc9879c2ab3e794f3c1237cc0
Niraj <neeraz.pandey2058@gmail.com>
2023-06-18 21:30:48 +0545

    Added Comment on models and admin.

--- iCoder/blog/urls.py
+++ iCoder/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blogHome, name='blogHome'),
+    path('blogComment', views.blogComment, name='blogComment'),
     path('<str:slug>', views.blogPost, name='blogPost')
 ]
--- iCoder/blog/views.py
+++ iCoder/blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, HttpResponse
-from .models import Post
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Post, BlogComment
+from django.contrib import messages
+
 
 # Create your views here.
-
-
 def blogHome(request):
     allPost = Post.objects.all()
     return render(request, "blog/bloghome.html", {'allPost': allPost})
@@ -11,5 +11,19 @@ def blogHome(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug)[0]
-    context = {'post':post}
+    comments = BlogComment.objects.filter(post=post)
+    context = {'post':post, 'comments':comments}
     return render(request, "blog/blogpost.html", context)
+
+def blogComment(request):
+    if request.method == "POST":
+        comment = request.POST.get('comment')
+        user = request.user
+        postSno = request.POST.get('postSno')
+        post = Post.objects.get(sno=postSno)
+
+        comment = BlogComment(comment=comment, user=user, post=post)
+        comment.save()
+        messages.success(request, 'Your comment has been posted!')
+
+    return redirect(f'/blog/{post.slug}')
